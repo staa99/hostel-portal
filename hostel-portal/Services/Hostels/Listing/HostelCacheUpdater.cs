4c5a97ca4053a2f38f2a824de7d53c838ffa3950
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2020-06-11 15:04:58 +0100

    implement hostel cache updater

---
+++ hostel-portal/Services/Hostels/Listing/HostelCacheUpdater.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Staawork.Funaab.HostelPortal.Commons.Caching;
+using Staawork.Funaab.HostelPortal.Services.Hostels.Listing.Config;
+using Staawork.Funaab.HostelPortal.Services.Hostels.Listing.Dto;
+using StackExchange.Redis;
+
+
+namespace Staawork.Funaab.HostelPortal.Services.Hostels.Listing
+{
+    internal class HostelCacheUpdater : IHostelCacheUpdater
+    {
+        private readonly IRedisCache _cache;
+
+
+        public HostelCacheUpdater(IRedisCache cache)
+        {
+            _cache = cache;
+        }
+
+
+        /// <inheritdoc />
+        public async Task UpdateHostels(IEnumerable<HostelDto> hostels)
+        {
+            var map = hostels.Select(h => new HashEntry(h.Id, JsonConvert.SerializeObject(h))).ToArray();
+            await _cache.Database.HashSetAsync(HostelListingConfiguration.HostelMapKey, map);
+        }
+    }
+}
