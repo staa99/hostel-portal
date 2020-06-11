9be26c6a5382e3815fefafebbf4bd01055929b57
Ahmad Alfawwaz <ahmadulfawwaz@gmail.com>
2020-06-11 15:04:12 +0100

    implement hostel loader

---
+++ hostel-portal/Services/Hostels/Listing/HostelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Staawork.Funaab.HostelPortal.Services.Hostels.Listing.Dto;
+
+
+namespace Staawork.Funaab.HostelPortal.Services.Hostels.Listing
+{
+    internal class HostelLoader : IHostelLoader
+    {
+        /// <inheritdoc />
+        public async Task<ICollection<HostelDto>> LoadHostels(IHostelCacheChecker cacheChecker,
+                                                              IHostelCacheUpdater cacheUpdater,
+                                                              IHostelWebApiLoader webApiLoader)
+        {
+            var hostels = await cacheChecker.LoadHostels();
+            if (hostels.Any())
+            {
+                return hostels;
+            }
+
+            hostels = await webApiLoader.LoadHostels();
+            if (hostels.Any())
+            {
+                await cacheUpdater.UpdateHostels(hostels);
+            }
+
+            return hostels;
+        }
+    }
+}
